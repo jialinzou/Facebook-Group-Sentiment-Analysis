717da6527b45adaaabb2acd6beaac05137ae548e
mxlei01 <mxlei01@gmail.com>
2015-11-30 00:05:03 -0700

    Changed services_only.sh to include --name for docker

--- Docker/Docker_startup_scripts/services_only.sh
+++ Docker/Docker_startup_scripts/services_only.sh
@@ -1,2 +1,2 @@
 cd ../../ && sudo docker build --file="Docker/Dockerfiles/Dockerfile_services_only" -t mxlei01/facebook-sentiment-analysis-services-only .
-sudo docker run --net="host" mxlei01/facebook-sentiment-analysis-services-only
+sudo docker run --net="host" --name mxlei01/facebook-sentiment-analysis-services-only mxlei01/facebook-sentiment-analysis-services-only
