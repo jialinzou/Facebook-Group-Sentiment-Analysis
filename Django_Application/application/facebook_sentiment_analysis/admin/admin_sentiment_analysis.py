e6a86461ba87417f773f3ccaa6998825b8489d42
mxlei01 <mxlei01@gmail.com>
2015-12-21 03:13:43 -0700

    Changed train

--- Django_Application/application/facebook_sentiment_analysis/admin/admin_sentiment_analysis.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from application.facebook_sentiment_analysis.models.sentiment_analysis.user_sentiment import UserSentiment
-
-# admin_sentiment_analysis.py is used for registering sentiment_analysis models to the administrator site
-
-admin.site.register(UserSentiment)
