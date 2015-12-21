2c85d01b0724bec13395d09ec36596872a54cbed
mxlei01 <mxlei01@gmail.com>
2015-12-20 18:27:04 -0700

    Trying to fix graphlab

--- Docker/Docker_startup_scripts/full_service.sh
+++ Docker/Docker_startup_scripts/full_service.sh
@@ -1,2 +1,3 @@
 cd ../../ && sudo docker build --file="Docker/Dockerfiles/Dockerfile" -t mxlei01/facebook-sentiment-analysis .
-sudo docker exec -i -t mxlei01/facebook-sentiment-analysis bash
+sudo docker run --net="host" --name facebook-sentiment-analysis mxlei01/facebook-sentiment-analysis &
+sleep 15 && sudo docker exec -i -t facebook-sentiment-analysis bash
