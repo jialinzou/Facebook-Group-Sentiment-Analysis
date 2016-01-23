4ecf865ab55e7a45cf09f34ff21912a47118e1c4
mxlei01 <mxlei01@gmail.com>
2016-01-23 14:53:31 -0700

    Added nginx service so that js and css files are served

--- Docker/Docker_startup_scripts/full_service.sh
+++ Docker/Docker_startup_scripts/full_service.sh
@@ -1,3 +1,5 @@
 cd ../../ && sudo docker build --file="Docker/Dockerfiles/Dockerfile" -t mxlei01/facebook-sentiment-analysis .
 sudo docker run --net="host" --name facebook-sentiment-analysis mxlei01/facebook-sentiment-analysis &
-sleep 15 && sudo docker exec -i -t facebook-sentiment-analysis bash
+cd ./Docker/Nginx && sudo docker build --file="Dockerfile" -t mxlei01/mxlei01-nginx .
+sudo docker run --net="host" --name mxlei01-nginx mxlei01/mxlei01-nginx &
+sleep 60 && sudo docker exec -i -t facebook-sentiment-analysis bash
